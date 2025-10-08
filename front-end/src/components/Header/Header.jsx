02e626f457f1bc5e9e5b5de30de5f6762389f5b1
Matheus Diógenes Amorim <matheusdiogenesdev@gmail.com>
2025-10-08 18:46:45 -0300

    retirando conteudo nao mais utilizado

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -5,10 +5,6 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Referral System - Vortex</h1>
-      <nav className={styles.nav}>
-        <a className={styles.link} href="/">Cadastro</a>
-        <a className={styles.link} href="/login">Entrar</a>
-      </nav>
     </header>
   )
 }
